0522e14f3afd81c8dd7d2b35f3777d74f98d2b22
Alex <halastoi@gmail.com>
2026-03-21 17:46:15 +0200

    fix pwa icons + remove duplicate manifest/sw

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 
-// Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-      .then(() => console.log('[Somnio] SW registered'))
-      .catch((e) => console.log('[Somnio] SW failed:', e))
-  })
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
